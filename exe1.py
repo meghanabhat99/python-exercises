2672a58d0b4b57b3b89d1c1d92e493f2d4e4ba8e
meghana <bhatmeghana99@gmail.com>
2018-06-05 11:37:12 +0530

    added some changes to exercise1

--- exe1.py
+++ exe1.py
@@ -1,4 +1,4 @@
-name=input("what is your name?")
-age=int(input("how old are you?"))
+name=input("what is your name? ")
+age=int(input("how old are you? "))
 year=str((2018-age)+100)
-print("you will be 100 in the year"+year)
+print((name+" will be 100 in the year:") +year)
